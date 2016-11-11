a43fcf7c492aaa4144820eef1d229b4a0fec8432
Jochen Niebuhr <jniebuhr@cfire.de>
2016-11-11 09:26:54 +0100

    Revert change to spec

--- spec/mail/part_spec.rb
+++ spec/mail/part_spec.rb
@@ -191,7 +191,7 @@ ENDPART
   end
   
   it "should correctly parse plain text raw source and not truncate after newlines - issue 208" do
-    plain_text = "\nFirst Line\n\nSecond Line\n\nThird Line\n\n"
+    plain_text = "First Line\n\nSecond Line\n\nThird Line\n\n"
     #Note: trailing \n\n is stripped off by Mail::Part initialization
     part = Mail::Part.new(plain_text)
     expect(part[:content_type].content_type).to eq 'text/plain'
